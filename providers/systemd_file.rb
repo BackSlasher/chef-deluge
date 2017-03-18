7a09e2913b715ace3b22df96ec383200cec02e64
nitz <nitz.raz@gmail.com>
2017-03-18 12:13:28 +0200

    Moved from defaults file to systemd

--- providers/systemd_file.rb
+++ providers/systemd_file.rb
@@ -6,6 +6,7 @@ def def_stuff(resource, action)
     source 'default_file.erb'
     variables ({
       user: new_resource.user,
+      group: new_resource.group,
       mask: new_resource.mask,
       config_dir: new_resource.config_dir,
       log_location: new_resource.log_location,
--- resources/systemd_file.rb
+++ resources/systemd_file.rb
@@ -2,6 +2,7 @@ actions :create, :delete
 default_action :create
 
 attribute :user
+attribute :group
 attribute :mask
 attribute :config_dir
 attribute :log_location
