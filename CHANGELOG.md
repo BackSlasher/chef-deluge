939ed4ec2477aa2ab1cca64de2acac31e2abbc0d
Nitzan Raz <nitz.raz@gmail.com>
2020-09-24 23:37:28 +0300

    v0.2.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,8 @@
+# 0.2.0
+
+* No more `deluge_setting`, only `deluge_settings`
+* Setting settings using Python and not deluge-console, meaning we can set multiple settings at once
+
 # 0.1.0
 
 Initial release of deluge
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'nitz.raz@gmail.com'
 license          'GPL v2'
 description      'Installs/Configures Deluge, the BitTorrent client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
 source_url 'https://github.com/BackSlasher/chef-deluge'
 issues_url 'https://github.com/BackSlasher/chef-deluge/issues'
